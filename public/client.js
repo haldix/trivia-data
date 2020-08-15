18357309ed66f18ea72f802e95c1833248e2ceb3
haldix <hjdpirate@yahoo.com>
2020-08-15 00:15:03 -0400

    add hide saved data funtionality, minor cleanup

--- public/client.js
+++ public/client.js
@@ -2,72 +2,79 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('form').addEventListener('submit', handleForm);
   const inputVals = document.querySelectorAll('input[type="text"]');
   inputVals.forEach((el) => el.addEventListener('blur', valLength));
-  document.getElementById('btn-list').addEventListener('click', showList);
-  const data = document.getElementById('data');
+  const btnList = document.getElementById('btn-list');
+  btnList.addEventListener('click', showList);
+  const savedData = document.getElementById('saved-data');
+  //let url = 'http://localhost:3000/trivia/';
+  let url = 'https://bible-trivia-data.herokuapp.com/trivia';
   wakeHeroku();
-});
 
-//let url = 'http://localhost:3000/trivia/';
-let url = 'https://bible-trivia-data.herokuapp.com/trivia';
+  async function handleForm(e) {
+    confirm('Save this Question?');
+    e.preventDefault();
+    let myForm = e.target;
+    let fd = new FormData(myForm);
+    form.reset();
+    //log form data contents
+    // for (let key of fd.keys()) {
+    //   console.log(key, fd.get(key));
+    // }
 
-async function handleForm(e) {
-  confirm('Save this Question?');
-  e.preventDefault();
-  let myForm = e.target;
-  let fd = new FormData(myForm);
-  form.reset();
-  //log form data contents
-  // for (let key of fd.keys()) {
-  //   console.log(key, fd.get(key));
-  // }
+    let jsonData = await convertFDtoJSON(fd);
 
-  let jsonData = await convertFDtoJSON(fd);
+    //send the request with the formdata
+    let h = new Headers();
+    h.append('Content-type', 'application/json');
 
-  //send the request with the formdata
-  let h = new Headers();
-  h.append('Content-type', 'application/json');
+    let req = new Request(url, {
+      headers: h,
+      body: jsonData,
+      method: 'POST',
+    });
 
-  let req = new Request(url, {
-    headers: h,
-    body: jsonData,
-    method: 'POST',
-  });
-
-  try {
-    const res = await fetch(req);
-    const data = await res.json();
-    console.log('Response from server');
-    console.log(data);
-  } catch (error) {
-    console.warn(error);
+    try {
+      const res = await fetch(req);
+      const data = await res.json();
+      console.log('Response from server');
+      console.log(data);
+    } catch (error) {
+      console.warn(error);
+    }
   }
-}
 
-function valLength(e) {
-  let val = e.target.value;
-  if (val.trim().length > 75) {
-    alert('Please limit input length to 75 characters maximum.');
+  function valLength(e) {
+    let val = e.target.value;
+    if (val.trim().length > 75) {
+      alert('Please limit input length to 75 characters maximum.');
+    }
+    return;
   }
-  return;
-}
 
-function convertFDtoJSON(formData) {
-  let obj = {};
-  for (let key of formData.keys()) {
-    obj[key] = formData.get(key);
+  function convertFDtoJSON(formData) {
+    let obj = {};
+    for (let key of formData.keys()) {
+      obj[key] = formData.get(key);
+    }
+    return JSON.stringify(obj);
   }
-  return JSON.stringify(obj);
-}
 
-async function showList() {
-  try {
-    const res = await fetch(url);
-    const questions = await res.json();
-    console.log(questions);
-    const html = questions
-      .map(
-        (q) =>
-          `<div class='card'>
+  let dataShow = false;
+  async function showList() {
+    if (dataShow) {
+      btnList.innerText = 'Show Saved Questions';
+      savedData.innerHTML = '';
+      dataShow = false;
+      return;
+    }
+    try {
+      const res = await fetch(url);
+      const questions = await res.json();
+      dataShow = true;
+      btnList.innerText = 'Hide Saved Questions';
+      const html = questions
+        .map(
+          (q) =>
+            `<div class='card'>
             <p class='question'>${q.question}</p>
             <ul class="list">
               <li class='item-correct'>${q.correct}</li>
@@ -77,21 +84,22 @@ async function showList() {
             </ul>
             <p class='difficulty'>difficulty: ${q.difficulty}</p>
           </div>`
-      )
-      .join('');
-    data.innerHTML = html;
-    console.log(html);
-  } catch (err) {
-    console.log(err);
+        )
+        .join('');
+      savedData.innerHTML = html;
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
 
-async function wakeHeroku() {
-  const res = await fetch(`${url}/wake`);
-  const json = await res.json();
-  const dbText = document.getElementById('db-text');
-  if (json.connected) {
-    dbText.innerText = 'ON';
-    dbText.classList.add('connected');
+  async function wakeHeroku() {
+    const res = await fetch(`${url}/wake`);
+    const json = await res.json();
+    const dbText = document.getElementById('db-text');
+    console.log('jsonC', json);
+    if (json.connected) {
+      dbText.innerText = 'ON';
+      dbText.classList.add('connected');
+    }
   }
-}
+});
